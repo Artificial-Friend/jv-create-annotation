95cc7d991420456d7ec5f23acc880f2ddf382778
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2021-01-08 12:39:11 +0200

    added UserDaoImpl usage

--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -5,6 +5,7 @@ import core.basesyntax.dao.BetDaoImpl;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.dao.UserDaoImpl;
 import core.basesyntax.model.Bet;
+import core.basesyntax.model.User;
 import java.util.Scanner;
 
 public class ConsoleHandler {
@@ -23,8 +24,12 @@ public class ConsoleHandler {
                 String[] betData = command.split(" ");
                 int value = Integer.parseInt(betData[0]);
                 double risk = Double.parseDouble(betData[1]);
+                System.out.println("Now enter your name");
+                String userName = scanner.nextLine();
                 Bet bet = new Bet(value, risk);
+                User user = new User(new java.util.Random().nextInt(), userName);
                 betDao.addBet(bet);
+                userDao.addUser(user);
             } catch (NumberFormatException | ArrayIndexOutOfBoundsException exception) {
                 System.out.println("input is incorrect");
             }
